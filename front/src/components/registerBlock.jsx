cdbe5cbb755a55e26531d61db09be1abbc9639d8
elouandacostapeixoto@gmail.com <elouandacostapeixoto@gmail.com>
2022-08-14 19:58:53 +0200

    [FIX] the className to the loginBlock and registerBlock, add style to the button

--- front/src/components/registerBlock.jsx
+++ front/src/components/registerBlock.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import logo from '../assets/netfion_logo.webp';
 
 function RegisterBlock () {
   return (
-    <div className="register-block">
-      <div className="register-block__top__container">
-        <img src="" alt="netfion-logo" />
+    <div className="login-register-block">
+      <div className="login-register-block__top_container">
+        <img src={logo} alt="netfion-logo" />
         <h1>Register</h1>
       </div>
-      <div className="register-block__form__container">
-        <form >
-          <div className="register-block__form__container__input">
+      <div className="login-register-block__form_container">
+        <form className="login-register-block__form_container__form">
+          <div className="login-register-block__form_container__form__input">
             <input type="text" placeholder="Username" />
           </div>
-          <div className="register-block__form__container__input">
+          <div className="login-register-block__form_container__form__input">
             <input type="text" placeholder="Email" />
           </div>
-          <div className="register-block__form__container__input">
+          <div className="login-register-block__form_container__form__input">
             <input type="password" placeholder="Password" />
           </div>
-          <div className="register-block__form__container__input">
-            <input type="password" placeholder="Password confirmation" />
+          <div className="login-register-block__form_container__form__input">
+            <input type="password" placeholder="Confirmation" />
           </div>
         </form>
-        <button className="register-block__form__container__button">register</button>
+        <button className="login-register-block__form_container__button">Register</button>
         <p>Already have an account? <Link to="/login">Login</Link></p>
       </div>
     </div>
